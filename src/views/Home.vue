8c165d5fdd17fc93f558bf6760b8ad9f23c702ec
Leandro Gongora <crisgongor7@gmail.com>
2022-02-18 11:05:59 -0400

    comentado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h2>Listado de estudiantes</h2>
-    <Read />
+    <!-- <Read /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Read from "@/components/ReadCrud.vue";
+// import Read from "@/components/ReadCrud.vue";
 
 export default {
   name: "Home",
-  components: {
-    Read,
-  },
+  // components: {
+  //   Read,
+  // },
 };
 </script>
